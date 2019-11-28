6c215570d76f6f1f9f981a31c7a131bc81aa2f02
luvetriya <39872941+luvetriya@users.noreply.github.com>
2019-11-28 23:54:09 +0200

    2

---
+++ pr2/main.py
@@ -0,0 +1,59 @@
+import cgi
+import datetime
+import webapp2
+
+from google.appengine.ext import ndb
+from google.appengine.api import users
+
+guestbook_key = ndb.Key('Guestbook', 'default_guestbook')
+
+class Greeting(ndb.Model):
+  author = ndb.UserProperty()
+  content = ndb.TextProperty()
+  date = ndb.DateTimeProperty(auto_now_add=True)
+
+
+class MainPage(webapp2.RequestHandler):
+  def get(self):
+    self.response.out.write('<html><body>')
+
+    greetings = ndb.gql('SELECT * '
+                        'FROM Greeting '
+                        'WHERE ANCESTOR IS :1 '
+                        'ORDER BY date DESC LIMIT 10',
+                        guestbook_key)
+
+    for greeting in greetings:
+      if greeting.author:
+        self.response.out.write('<b>%s</b> wrote:' % greeting.author.nickname())
+      else:
+        self.response.out.write('An anonymous person wrote:')
+      self.response.out.write('<blockquote>%s</blockquote>' %
+                              cgi.escape(greeting.content))
+
+
+    self.response.out.write("""
+          <form action="/sign" method="post">
+            <div><textarea name="content" rows="3" cols="60"></textarea></div>
+            <div><input type="submit" value="Sign Guestbook"></div>
+          </form>
+        </body>
+      </html>""")
+
+
+class Guestbook(webapp2.RequestHandler):
+  def post(self):
+    greeting = Greeting(parent=guestbook_key)
+
+    if users.get_current_user():
+      greeting.author = users.get_current_user()
+
+    greeting.content = self.request.get('content')
+    greeting.put()
+    self.redirect('/')
+
+
+app = webapp2.WSGIApplication([
+  ('/', MainPage),
+  ('/sign', Guestbook)
+], debug=True)
